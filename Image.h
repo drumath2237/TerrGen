6f4a03f80ea684191dc6f932484a4c074305cf43
drumath <2237k.drumath@gmail.com>
2020-01-20 01:18:47 +0900

    :construction: ppm_out関数の実装WIP

--- Image.h
+++ Image.h
@@ -1,4 +1,6 @@
 #include <vector>
+#include <string>
+#include <fstream>
 
 namespace TerrGen {
   struct Color {
@@ -10,9 +12,17 @@ namespace TerrGen {
   
   struct Image {
     int w,h;
-    std::vector<Color> pixels;
+    std::vector<Color&> pixels;
 
     Image(int w_, int h_):w(w_),h(h_){}
     Image():w(800),h(800){}
+
+    void SetPixel(int x, int y, const Color& color) {
+      pixels[x+y*w] = color;
+    }
+
+    void ppm_out(std::string filename) const {
+      std::fstream file(filename);
+    }
   };
 };
