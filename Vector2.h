25e9213459dc4892f2e645e473bde4270b5a40d8
drumath <2237k.drumath@gmail.com>
2020-01-19 16:25:38 +0900

    :sparkles: 演算子を追加

--- Vector2.h
+++ Vector2.h
@@ -13,5 +13,34 @@ namespace TerrGen {
     double length2() {
       return x*x+y*y;
     }
+
+    Vector2 operator +(const Vector2& v) {
+      return Vector2(x+v.x, y+v.y);
+    }
+    Vector2 operator -(const Vector2& v) {
+      return Vector2(x-v.x, y-v.y);
+    }
+    Vector2 operator *(const Vector2& v) {
+      return Vector2(x*v.x, y*v.y);
+    }
+    Vector2 operator *(double d) {
+      return Vector2(x*d, y*d);
+    }
+    Vector2 operator /(const Vector2& v) {
+      if(v.x!=0 && v.y!=0)
+        exit(1);
+
+      return Vector2(x/v.x, y/v.y);
+    }
+    Vector2 operator /(double d) {
+      if(d!=0)
+        exit(1);
+
+      return Vector2(x/d, y/d);
+    }
   };
+
+  Vector2 operator *(double d, const Vector2& v) {
+    return Vector2(d*v.x, d*v.y);
+  }
 };
