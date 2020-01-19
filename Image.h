26bd77d7a960369046dbb8e807541384cd87d2a1
drumath <2237k.drumath@gmail.com>
2020-01-19 16:48:44 +0900

    :sparkles: Color概念の導入
    vector3にしとけばよかったと思う

--- Image.h
+++ Image.h
@@ -1,6 +1,16 @@
+#include <vector>
+
 namespace TerrGen {
+  struct Color {
+    double r,g,b;
+
+    Color(int r_, int g_, int b_):r(r_),g(g_),b(b_){}
+    Color():r(0),g(0),b(0){}
+  };
+  
   struct Image {
     int w,h;
+    std::vector<Color> pixels;
 
     Image(int w_, int h_):w(w_),h(h_){}
     Image():w(800),h(800){}
