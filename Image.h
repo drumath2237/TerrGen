fcd10d2f589cfc8ceb402d1bad53ddebe9aefbf4
drumath <2237k.drumath@gmail.com>
2020-01-20 18:03:54 +0900

    :sparkles: Colorについてちょっとした変更

--- Image.h
+++ Image.h
@@ -8,16 +8,17 @@ namespace TerrGen {
 
     Color(int r_, int g_, int b_):r(r_),g(g_),b(b_){}
     Color():r(0),g(0),b(0){}
+    Color(int c):r(c),g(0),b(0){}
   };
   
   struct Image {
     int w,h;
-    std::vector<Color&> pixels;
+    std::vector<Color> pixels;
 
     Image(int w_, int h_):w(w_),h(h_){}
     Image():w(800),h(800){}
 
-    void SetPixel(int x, int y, const Color& color) {
+    void SetPixel(int x, int y, const Color color) {
       pixels[x+y*w] = color;
     }
 
