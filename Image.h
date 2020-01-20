125566db3f154b4d29e4e795d1c307bea7ae7f5f
drumath <2237k.drumath@gmail.com>
2020-01-20 18:25:26 +0900

    :sparkles: ppm_out実装できたかも

--- Image.h
+++ Image.h
@@ -15,15 +15,30 @@ namespace TerrGen {
     int w,h;
     std::vector<Color> pixels;
 
-    Image(int w_, int h_):w(w_),h(h_){}
-    Image():w(800),h(800){}
+    Image(int w_, int h_):w(w_),h(h_){ pixels = std::vector<Color>(w_*h_); }
+    Image():w(400),h(400){ pixels = std::vector<Color>(400*400); }
 
     void SetPixel(int x, int y, const Color color) {
       pixels[x+y*w] = color;
     }
 
+    Color GetPixel(int x, int y) const {
+      return pixels[x+y*w];
+    }
+
     void ppm_out(std::string filename) const {
       std::fstream file(filename);
+
+      file << "P3" << std::endl;
+      file << w << " " << h << std::endl;
+      file << "255" << std::endl;
+
+      for(int y=0; y<h; y++) for(int x=0; x<w; x++) {
+        file << GetPixel(x,y).r << " "
+             << GetPixel(x,y).g << " "
+             << GetPixel(x,y).b << " "
+             << std::endl;
+      }
     }
   };
 };
