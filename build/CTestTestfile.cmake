17f77cee4ead6524499e378f9ecb1c59408cc84e
drumath <2237k.drumath@gmail.com>
2020-01-22 23:38:19 +0900

    :boom: ノリでcmake導入したらデバッグできたw

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/kaitoFolder/programs/terrain gen
+# Build directory: D:/kaitoFolder/programs/terrain gen/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
